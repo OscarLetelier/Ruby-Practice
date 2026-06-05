fc708ad643c9db94a7820a2d4bce7e594be9f98b
Oscar Letelier Molina <oscar.letelier@outlook.com>
2026-06-04 20:17:42 -0400

    refactor: apply before_action callback to DRY up SupportTicketsController

--- rails_project/my_app/app/controllers/support_tickets_controller.rb
+++ rails_project/my_app/app/controllers/support_tickets_controller.rb
@@ -1,36 +1,29 @@
 class SupportTicketsController < ApplicationController
+  before_action :set_ticket, only: [:edit, :update, :destroy]
+
   def index
     @tickets = SupportTicket.all
   end
 
-  # 1. Acción NEW: Prepara un "molde" vacío para el formulario
   def new
     @ticket = SupportTicket.new
   end
 
-  # 2. Acción CREATE: Recibe los datos del botón "Submit" y los guarda
   def create
     @ticket = SupportTicket.new(ticket_params)
 
     if @ticket.save
-      # Si se guarda en PostgreSQL, redireccionamos a la página principal
       redirect_to root_path
     else
-      # Si hay un error, volvemos a mostrar la vista "new" con los datos que ya había escrito
       render :new, status: :unprocessable_entity
     end
   end
 
-  # 3. Accion EDIT: Busca el ticket en la DB para llenar el formulario
   def edit
-    @ticket = SupportTicket.find(params[:id])
+    @ticket
   end
 
-  # 4. Accion UPDATE: Procesa los cambios enviados por el usuario
   def update
-    @ticket = SupportTicket.find(params[:id])
-
-    # En lugar de .new, usamos .update pasando nuestros Strong Parameters
     if @ticket.update(ticket_params)
       redirect_to root_path
     else
@@ -38,9 +31,16 @@ class SupportTicketsController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    @ticket.destroy
+    redirect_to root_path, status: :see_other
+  end
 
-  # STRONG PARAMETERS: El filtro de seguridad de la aduana
+  private
+  def set_ticket
+    @ticket = SupportTicket.find(params[:id])
+  end
+
   def ticket_params
     params.require(:support_ticket).permit(:customer_name, :issue_description, :status)
   end
